ce07a4648c50440c1b7ba44d9c1b107edc1a0c97
miyazakisayaka7 <une_charrue@yahoo.co.jp>
2021-04-24 16:36:54 +0900

    ログインしていなくてもできるアクション

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!, except: [:index, :show] do
+  end
     
   def new
     @product = Product.new
