11a0d9d4074acc5b6e165adb51d09009e5c23c42
miyazakisayaka7 <une_charrue@yahoo.co.jp>
2021-04-25 19:27:38 +0900

    商品削除機能修正①

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_product, only: [:edit, :show, :update]
-  before_action :set_products, only: [:edit, :update]
+  before_action :set_product, only: [:edit, :show, :update, :destroy]
+  before_action :set_products, only: [:edit, :update, :destroy]
 
   def new
     @product = Product.new
@@ -36,7 +36,6 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-    product = Product.find(params[:id])
     if product.destroy
     redirect_to root_path
     end
