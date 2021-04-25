f4c429dabc6024a6741b09fc45f1166e9a8df9b1
miyazakisayaka7 <une_charrue@yahoo.co.jp>
2021-04-25 16:09:10 +0900

    商品情報編集修正⑥

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_product, only: [:edit, :show, :update]
-  before_action :set_products, only; [:edit, :update]
+  before_action :set_products, only: [:edit, :update]
 
   def new
     @product = Product.new
