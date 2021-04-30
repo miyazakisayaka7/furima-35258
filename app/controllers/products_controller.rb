f07de1e5cb94d1f3e71f746a3a3e26c10da61ab6
miyazakisayaka7 <une_charrue@yahoo.co.jp>
2021-04-30 11:44:30 +0900

    商品購入機能修正⑥

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,6 @@ class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_product, only: [:edit, :show, :update, :destroy]
   before_action :set_products, only: [:edit, :update, :destroy]
-  before_action :set_purchase_present, only: [:edit, :update]
   def new
     @product = Product.new
   end
@@ -53,13 +52,7 @@ private
   end
 
   def set_products
-    unless current_user.id == @product.user_id
-      redirect_to root_path
-    end
-  end
-
-  def set_purchase_present
-    if @product.purchase.present?
+    unless current_user.id == @product.user_id or @product.purchase.present?
       redirect_to root_path
     end
   end
